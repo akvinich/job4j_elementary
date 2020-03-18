c032fac5d962c7598a3e512746f684173e4d44bc
vitali <vital1234mail@gmail.com>
2020-03-18 20:06:24 +0300

    fixed 4.4. Максимум из трех чисел[#251902]

--- chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
+++ chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
@@ -2,6 +2,7 @@ package ru.job4j.condition;
 
 public class MultiMax {
     public int max(int first, int second, int third) {
-        return first >= second && first >= third ? first : (second >= third ? second : third);
+        int firstSecondResult = first >= second ? first : second;
+        return firstSecondResult >= third ? firstSecondResult : third;
     }
 }
