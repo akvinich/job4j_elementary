8f9e09af7b29243770959a20a901803422b31b65
vitali <vital1234mail@gmail.com>
2020-03-25 21:16:18 +0300

    6.0.2. Заполнение массива.[#251923]

--- chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -6,5 +6,14 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Vitali";
+        names[1] = "Dima";
+        names[2] = "Olga";
+        names[3] = "Vlad";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
