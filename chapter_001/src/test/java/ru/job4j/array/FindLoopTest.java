69f14c66975698f2bc16b502f1e700422b030e56
vitali <vital1234mail@gmail.com>
2020-03-29 18:40:16 +0300

    6.5. Поиск индекса в диапазоне.[#251916]

--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -25,4 +25,48 @@ public class FindLoopTest {
         int expect = -1;
         assertThat(result, is(expect));
     }
+
+    @Test
+    public void whenFind3() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenFindFrom0To1Negative() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 10;
+        int start = 0;
+        int finish = 1;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenFindFrom0To1Positive() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 0;
+        int finish = 1;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 1;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenFindEndOfMassPositive() {
+        int[] input = new int[] {5, 2, 10, 2, 4, 7};
+        int value = 7;
+        int start = 4;
+        int finish = 5;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 5;
+        assertThat(result, is(expect));
+    }
 }
