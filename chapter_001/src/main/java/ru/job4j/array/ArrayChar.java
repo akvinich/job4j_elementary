dc124a2a8b69688ec05957e1114b5ecfceceea41
vitali <vital1234mail@gmail.com>
2020-04-02 13:21:44 +0300

    6.4. Слово начинается с ...[#251911] fixed

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -6,7 +6,6 @@ public class ArrayChar {
 
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
-        Assert.assertEquals(true, word.length >= pref.length);
         for (int i = 0; i < pref.length; i ++) {
             if (pref[i] != word[i]) {
                 result = false;
