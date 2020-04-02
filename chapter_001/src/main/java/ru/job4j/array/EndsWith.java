0a18836b1f2c0a91098190da0689a36b32db2f7e
vitali <vital1234mail@gmail.com>
2020-04-02 13:26:12 +0300

    6.4.1. Слово заканчивается с ...[#251915] fixed

--- chapter_001/src/main/java/ru/job4j/array/EndsWith.java
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -5,9 +5,8 @@ import org.junit.Assert;
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = true;
-        Assert.assertEquals(true, word.length >= post.length);
-        for (int i = post.length - 1, j = word.length - 1; i >= 0; j--, i--) {
-            if (post[i] != word[j]) {
+        for (int postEnd = post.length - 1, wordEnd = word.length - 1; postEnd >= 0; wordEnd--, postEnd--) {
+            if (post[postEnd] != word[wordEnd]) {
                 result = false;
                 break;
             }
